4df36d91990fd650ce238d23f8541b7a3365995c
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2023-09-26 10:22:50 +0400

    Delete weekFour/Main.java

--- weekFour/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		CreditCard card = new CreditCard();
-		card.CreditCard("Charlie Doug", "4034 3434 3556 5656", 1345);
-		card.display();
-	}
-}
