7bc2da4222156402f4f214a44642636b5c33df76
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2023-10-24 10:14:02 +0400

    Update PhoneNumber.java

---
+++ L9/PhoneNumber.java
@@ -0,0 +1,23 @@
+Week 9 Question 3:
+PhoneNumberDetails.java
+import java.util.Scanner;
+
+public class PhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a phone number: ");
+        String phoneNumber = scanner.nextLine();
+
+        String countryCode = phoneNumber.substring(0, 5);
+        System.out.println("Country Code: " + countryCode);
+
+        if (phoneNumber.contains("55")) {
+            System.out.println("Mobile Phone");
+        } else if (phoneNumber.contains("4")) {
+            System.out.println("Landline");
+        }
+
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
