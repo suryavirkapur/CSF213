f4d086492f683e500cc285f2e9f997c232b78c3e
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2023-10-24 09:30:04 +0400

    Update Flights.java

--- L8/Flights.java
+++ L8/Flights.java
@@ -1,17 +1,70 @@
-class FlightDest {
-	 private String flightNo = "";
-	 private String destination = "";
+import java.util.Scanner;
+import java.util.ArrayList;
 
-	 public FlightDest(String flightNo, String destination) {
-		this.flightNo = flightNo;
-		this.destionation = destination;
- 	}
-	
-	public getFlightNo() { return this.flightNo; }
-	public getDestionation() { return this.destination; } 
+class FlightDest {
+    private String flightNo, destination;
+    FlightDest(String flightNo, String destination) {
+        this.flightNo = flightNo;
+        this.destination = destination;
+    }
+    String getFlightNo() {return flightNo;}
+    String getDest() {return destination;}
 }
 
-public class FlightInfo {
-	FlightDest[] flightDestList = {};
-	
+class FlightInfo {
+
+    ArrayList<FlightDest> flightDestList = new ArrayList<FlightDest>();
+    
+    public void addFlightDestPair(String fNo, String dest) {
+        flightDestList.add(new FlightDest(fNo, dest));
+    }
+
+    public ArrayList<String> getFlights(String dest) {
+        ArrayList<String> flights = new ArrayList<String>();
+        for (FlightDest flight : flightDestList) {
+            if (flight.getDest().equals(dest)) {
+                flights.add(flight.getFlightNo());                
+            }
+        }
+        return flights;
+    }
+
+}
+
+public class Flights {
+
+    public static void main(String args[]) {
+
+        int choice = 0;
+        FlightInfo fi = new FlightInfo();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (choice != 3) {
+
+            System.out.println("Enter choice:\n1. Add Flight-Destination Pair\n2. Get Flights for Destination\n3. Exit\n");
+            choice = Integer.parseInt(sc.nextLine());
+
+            switch (choice) {
+
+                case 1: {
+                    System.out.println("Enter Flight and Destination separately");
+                    fi.addFlightDestPair(sc.nextLine(), sc.nextLine());
+                    break;
+                }
+
+                case 2: {
+                    System.out.print("Enter Destination = ");
+                    System.out.println(fi.getFlights(sc.nextLine()));
+                    break;
+                }
+
+            }
+
+            System.out.println("");
+
+        }
+
+    }
+
 }
