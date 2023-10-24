16e36b1909a03d5d3fec22f21688429e6fc36cee
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2023-10-24 10:42:55 +0400

    Update Anagram.java

---
+++ L9/Anagram.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first string: ");
+        String str1 = scanner.nextLine();
+
+        System.out.print("Enter the second string: ");
+        String str2 = scanner.nextLine();
+
+        str1 = str1.replaceAll("[\\s\\p{Punct}]", "");
+        str2 = str2.replaceAll("[\\s\\p{Punct}]", "");
+
+        boolean areAnagrams = checkAnagrams(str1, str2);
+
+        if (areAnagrams) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+    }
+
+    private static boolean checkAnagrams(String str1, String str2) {
+
+        char[] charArray1 = str1.toLowerCase().toCharArray();
+        char[] charArray2 = str2.toLowerCase().toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
