9f180a2cd398eced495a4ea058ba09f8923749ce
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2023-10-24 10:11:40 +0400

    Update IdentityConstructor.java

--- L9/IdentityConstructor.java
+++ L9/IdentityConstructor.java
@@ -1,15 +1,33 @@
+
 import java.util.Random;
+import java.util.Scanner;
 
 public class IdentityConstructor {
-    // 2077 A7 PS 7777 U
-    // YEAR
-    // PS/TS
-    // Campus {Pilani P, Hyderabad H, Goa G, Dubai U}
-    // Random 3 Digits
-    static main() {
-        Random rand = new Random();
-        int n = rand.nextInt(1000);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.print("Enter Year: ");
+        int year = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter Discipline (CHEM-A1, EEE-A3, MECH-A4, CS-A7): ");
+        String discipline = scanner.nextLine().toUpperCase();
+
+        System.out.print("Enter PS/TS: ");
+        String psOrTs = scanner.nextLine().toUpperCase();
+
+        System.out.print("Enter Campus (Pilani P, Hyderabad H, Goa G, Dubai U): ");
+        String campus = scanner.nextLine().toUpperCase();
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(899) + 100;
+
+        String studentId = String.format("%d%s%s%03d%s", year, discipline.charAt(0), discipline.substring(1), randomNumber, campus.charAt(0));
+
+
+        String emailId = String.format("f%d@%s.bits-pilani.ac.in", year, campus.toLowerCase());
+
+        System.out.println("BITS Student ID: " + studentId);
+        System.out.println("BITS Email ID: " + emailId);
     }
 }
