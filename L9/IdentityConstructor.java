42f78a93e0c2c082098c8fea55737ba12dcc90c7
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2023-10-24 10:04:28 +0400

    Create IdentityConstructor.java

---
+++ L9/IdentityConstructor.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class IdentityConstructor {
+    // 2077 A7 PS 7777 U
+    // YEAR
+    // PS/TS
+    // Campus {Pilani P, Hyderabad H, Goa G, Dubai U}
+    // Random 3 Digits
+    static main() {
+        Random rand = new Random();
+        int n = rand.nextInt(1000);
+
+        
+    }
+}
